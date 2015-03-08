9a0b6896c4682f4c96062bb9dc6d5249e11b014a
Filipi <you@example.com>
2015-03-08 16:05:32 -0300

    Resolvido exercicio de multiplicacao entre matrizes

--- matrizes/exercicio03.c
+++ matrizes/exercicio03.c
@@ -22,40 +22,68 @@
 
 int main()
 {
-    int l, c;
+    int l, c, i, resultadoAux = 0;
     int matriz[2][3], matriz1[3][4], resultado[2][4];
     
-    printf("\n\nLendo a matriz e mostrando sua transposta \n");
-    for(l=0; l<4; l++)
+    printf("\n\nLendo a primeira matriz \n");
+    for(l=0; l<2; l++)
+    {
+        for(c=0; c<3; c++)
+        {
+            printf("\nEntre com o elemento da linha %d e coluna %d: ", l, c);
+            scanf("%d", &matriz[l][c]);
+            
+        }
+    }
+    printf("\n\nLendo a segunda matriz \n");
+    for(l=0; l<3; l++)
     {
         for(c=0; c<4; c++)
         {
             printf("\nEntre com o elemento da linha %d e coluna %d: ", l, c);
-            scanf("%d", &matriz[l][c]);
+            scanf("%d", &matriz1[l][c]);
         }
     }
-    for(l=0; l< 4; l++)
+
+    printf("\n\n\nPrimeira Matriz\n\n\n");
+    for(l=0; l< 2; l++)
     {
-        for(c=0; c<4; c++)
-        {
-            transp[c][l] = matriz[l][c];
-        }
-    }
-    
-    for(l=0; l< 4; l++)
-    {
-        for(c=0; c<4; c++)
+        for(c=0; c<3; c++)
         {
             printf("%5d ",matriz[l][c]);
         }
         printf("\n");
     }
     
-    for(l=0; l< 4; l++)
+    printf("\n\n\n\nSegunda Matriz\n\n\n\n");
+    for(l=0; l< 3; l++)
     {
         for(c=0; c<4; c++)
         {
-            printf("%5d ",transp[l][c]);
+            printf("%5d ",matriz1[l][c]);
+        }
+        printf("\n");
+    }
+    
+    for(l=0; l<2; l++)
+    {
+        for(c=0; c<4; c++)
+        {
+            for(i=0; i<3; i++)
+            {
+                resultadoAux += matriz[l][i] * matriz1[i][c];
+            }
+            resultado[l][c] = resultadoAux;
+            resultadoAux = 0;
+        }
+    }
+    
+    printf("\n\n\n\nResultado da multiplicacao\n\n\n\n");
+    for(l=0; l< 2; l++)
+    {
+        for(c=0; c<4; c++)
+        {
+            printf("%5d ",resultado[l][c]);
         }
         printf("\n");
     }
